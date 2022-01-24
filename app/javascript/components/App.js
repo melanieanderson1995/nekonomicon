8c1c7b7e5c5568f079a221451468999b68c6af26
Melanie Anderson <melaneanderson1995@gmail.com>
2022-01-24 13:54:57 -0500

    wip

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Breeds from '../components/Breeds'
  
 class App extends React.Component {
   render() {
     return (
         <div>
-            Hello World
+            <div className="text-lg text-white text-center">
+                He took a dusty book off the shelf..."Nekonomicon" in old carved letters into the leather. The book appeared to breathe and shift of its own accord, a living tome. Turn the page and take a look...
+            </div>
+            <div className="flex-1 flex justify-center items-center p-4">
+                <Link to="/breeds">
+                    <button className="btn btn-yellow">Breeds</button>
+                </Link>
+                <Link to="/cats">
+                    <button className="btn btn-purple">Cats</button>
+                </Link>
+            </div>
         </div>
     )
   }
