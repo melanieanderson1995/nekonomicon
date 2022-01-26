9b7750e72090b34dfd573c6b92cdeb219c738a12
Melanie Anderson <melaneanderson1995@gmail.com>
2022-01-26 13:16:14 -0500

    Pulling ruby edit form into react, working on delete functionality

--- app/javascript/components/Breeds.js
+++ app/javascript/components/Breeds.js
@@ -1,12 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
  
-const Breeds = () => {
-  return (
-    <div>
-      <h1>Breeds</h1>
-      <p>This is our breeds page.</p>
-    </div>
-  )
+const Breeds = ({auth_token}) => {
+    const [breeds, setBreeds] = useState([]);
+
+    console.log("AUTH", auth_token);
+
+    useEffect(() => {
+        const url = "/breeds.json";
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then(response => setBreeds(response))
+            .catch((error) => console.log(error));
+        }, []);
+
+    const handleDelete = (item) => {
+      console.log("BREEDID", item);
+      const url = `/breeds/${item}`;
+      console.log("URL", url);
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': auth_token
+        }})
+          .then(response => {
+              console.log("RESPONSE", response);
+              if (response.ok) {
+                  return response.json();
+              }
+              throw new Error("Network response was not ok.");
+          })
+          .catch((error) => console.log(error));
+    }
+
+    return ( 
+        <div className="card-wrapper-container">
+            <div className="card-title-container">
+                <span className="card-title">
+                Breeds
+                </span>
+            </div>
+            <div className="card-main-container flex flex-col">
+                <table className="grid-table mx-10 my-3 table-fixed">
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Rare</th>
+                    <th>Image url</th>
+                    <th className="text-center w-64">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {breeds.map((breed) => (
+                        <tr className="p-2">
+                            <td>{breed.name}</td>
+                            <td className="truncate w-96 flex">{breed.description}</td>
+                            <td>{breed.rare}</td>
+                            <td>{breed.image_url}</td>
+                            <td className="justify-center flex">
+                              <a href={`/breeds/${breed.id}`} className="btn btn-purple">Show</a>
+                              <a href={`/breeds/${breed.id}/edit`} className="btn btn-yellow">Edit</a>
+                              <button className="btn btn-red" onClick={() => handleDelete(breed.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+                </table>
+                <div className="justify-end flex">
+                    <a href={"/breeds/new"} className="btn btn-purple m-2 w-40">+ New Breed</a>
+                </div>
+            </div>
+        </div>
+    )
 }
  
 export default Breeds
---
+++ app/javascript/components/EditBreed.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+
+const EditBreed = ({auth_token, breed}) => {
+    const [name, setName] = useState(breed.name);
+    const [rare, setRare] = useState(breed.rare);
+    const [desc, setDesc] = useState(breed.description);
+    const [image, setImage] = useState(breed.image_url);
+    const values = {
+        name: name,
+        rare: rare,
+        description: desc,
+        image_url: image
+    }
+
+    useEffect(() => {
+        console.log("CURRENT", breed);
+    }, [breed])
+
+    const handleSubmit = (event) => {
+        console.log("EVENT", event);
+        const url = `/breeds/${breed.id}`
+        fetch(url, {
+            method: "PUT",
+            headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': auth_token
+            },
+            body: JSON.stringify(values)
+        }).then(response => {
+            if (response.ok) {
+            console.log("UPDATE SUCCESS")
+            }
+            throw new Error("Network update response was not ok");
+        }).catch((error) => console.log(error));
+    }
+
+//   <% if breed.errors.any? %>
+//     <div id="error_explanation">
+//       <h2><%= pluralize(breed.errors.count, "error") %> prohibited this breed from being saved:</h2>
+
+//       <ul>
+//         <% breed.errors.each do |error| %>
+//           <li><%= error.full_message %></li>
+//         <% end %>
+//       </ul>
+//     </div>
+//   <% end %>
+
+    const handleChange = (e) => {
+        const target = e.target;
+        const value = target.checked;
+
+        setRare(value);
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div className="field my-2">
+                <label className="text-white font-bold">Name: </label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="bg-gray-800 text-white p-2 rounded border border-gray-600 w-full" />
+            </div>
+            <div className="field flex flex-row">
+                <label className="text-white font-bold">Rare: </label>
+                <input type="checkbox" checked={rare} onChange={handleChange} />
+            </div>
+            <div className="field mb-2">
+                <label className="text-white font-bold">Description: </label>
+                <input type="textarea" value={desc} onChange={(e) => setDesc(e.target.value)} className="bg-gray-800 text-white p-2 rounded border border-gray-600 w-full" />
+            </div>
+            <div className="field mb-2">
+                <label className="text-white font-bold">Image URL: </label>
+                <input type="url" value={image} onChange={(e) => setImage(e.target.value)} className="bg-gray-800 text-white p-2 rounded border border-gray-600 w-full" />
+            </div>
+            <input type="submit" className="btn btn-green"  />
+        </form>
+    )
+}
+
+export default EditBreed
